77e506915f78bc1799a36678321adea1bcffe151
alfa14290 <angular.aditya@gmail.com>
2017-04-19 22:55:58 +0530

    Mobile View Done

--- main.js
+++ main.js
@@ -1,29 +1,118 @@
-$(document).ready(function () {
 
-  
-  //For calculaton of screen width and height
+$(document).ready(function(){
+//---index Page Start---// 
 
-  var scrollTop = 0;
-  var scrollLeft = 0;
-
-  $(window).scroll(function(){
-  scrollTop = $(window).scrollTop();
-  scrollLeft = $(window).scrollLeft();
-  var window_width =$(window).width();
-  var b =$( window ).height();
-
-  
-
-  if(scrollTop>1){
-    value=0.65;
-    $('.navbar-default').css('background','rgba(0,0,0,'+value+')')
-    $('..navbar-default .navbar-nav > li > .dropdown-menu > li > a ').css('background','rgba(0,0,0,'+value+')')
-  }
-  else{
-    $('.navbar-default').css('background','transparent')
-    $('..navbar-default .navbar-nav > li > .dropdown-menu > li > a ').css('background','transparent')
-  }
+    $("#SubmitButton").hover(function(){
     
 
+if( $('#EmailField').val() == "" ) {
+         
+         $("#EmailField:first").css({"border": "1px solid #ff0000"});
+         //$("#SubmitButton:first").removeClass("hvr-grow-shadow");
+         $("#SubmitButton:first").addClass("hvr-buzz-out");
+        }
+else{
+   
+   $("#SubmitButton:first").removeClass("hvr-buzz-out");
+   $("#SubmitButton:first").addClass("hvr-grow-shadow");
+}
+        }, function(){
+          $("#EmailField:first").css({"border": ""});
+        $("#SubmitButton:first").removeClass("hvr-buzz-out");
+    });
+
+//---index Page Ends---//
+
+//---ImportVideo Page Start//
+
+$("#ImportButton").hover(function(){
+    
+
+if( $('#ImportField').val() == "" ) {
+         
+         $("#ImportField:first").css({"border": "1px solid #ff0000"});
+         //$("#SubmitButton:first").removeClass("hvr-grow-shadow");
+         $("#ImportButton:first").addClass("hvr-buzz-out");
+        }
+else{
+   
+   $("#ImportButton:first").removeClass("hvr-buzz-out");
+   $("#ImportButton:first").addClass("hvr-grow-shadow");
+}
+        }, function(){
+          $("#ImportField:first").css({"border": ""});
+        $("#ImportButton:first").removeClass("hvr-buzz-out");
+    });
+
+
+//---ImportVideo Page Ends//
+
+//--ImportVideo2 Page Starts//
+
+$("#ImportButton1").hover(function(){
+    
+
+if( $('#ImportField1').val() == "" ) {
+         
+         $("#ImportField1:first").css({"border": "1px solid #ff0000"});
+         //$("#SubmitButton:first").removeClass("hvr-grow-shadow");
+         $("#ImportButton1:first").addClass("hvr-buzz-out");
+        }
+else{
+   
+   $("#ImportButton1:first").removeClass("hvr-buzz-out");
+   $("#ImportButton1:first").addClass("hvr-grow-shadow");
+}
+        }, function(){
+          $("#ImportField1:first").css({"border": ""});
+        $("#ImportButton1:first").removeClass("hvr-buzz-out");
+    });
+
+
+
+
+//---ImportVideo2 Page Ends//
+
+
+//---Add Cta page Start---//
+
+$("#DoneButton").hover(function(){
+    
+    if( ($('#EmailField').val() == "")  && ($('#WebsiteField').val() == "" ) ) {
+         
+         $("#EmailField:first").css({"border": "1px solid #ff0000"});
+         $("#WebsiteField:first").css({"border": "1px solid #ff0000"});
+         //$("#SubmitButton:first").removeClass("hvr-grow-shadow");
+         $("#DoneButton:first").addClass("hvr-buzz-out");
+        }
+
+        else if( $('#EmailField').val() == "" ) {
+         
+         $("#EmailField:first").css({"border": "1px solid #ff0000"});
+         //$("#SubmitButton:first").removeClass("hvr-grow-shadow");
+         $("#DoneButton:first").addClass("hvr-buzz-out");
+        }
+        else if( $('#WebsiteField').val() == "" ) {
+         
+         $("#WebsiteField:first").css({"border": "1px solid #ff0000"});
+         //$("#SubmitButton:first").removeClass("hvr-grow-shadow");
+         $("#DoneButton:first").addClass("hvr-buzz-out");
+        }
+
+else{
+   
+   $("#DoneButton:first").removeClass("hvr-buzz-out");
+   $("#DoneButton:first").addClass("hvr-grow-shadow");
+}
+        }, function(){
+          $("#EmailField:first").css({"border": ""});
+          $("#WebsiteField:first").css({"border": ""});
+        $("#DoneButton:first").removeClass("hvr-buzz-out");
+    });
+
+//--Add Cta Page Ends---//
+
+
+
 });
-});
+
